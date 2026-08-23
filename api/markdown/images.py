7e2b4cc6d4c42bd205330d452dadee58f2f12ffa
GSGFs <gsgfs@gsgfs.moe>
2026-08-23 22:50:31 +0800

    fix: remove IMAGE_PICTURE_URL_PREFIXES cache

--- api/markdown/images.py
+++ api/markdown/images.py
@@ -1,5 +1,4 @@
 from collections.abc import Iterable
-from functools import lru_cache
 
 from django.conf import settings
 from markdown_it_rs_py import ImageMetadata
@@ -7,7 +6,6 @@ from markdown_it_rs_py import ImageMetadata
 from media_service.models import ImageResource
 
 
-@lru_cache(1)
 def _image_picture_source_prefixes() -> tuple[str, ...]:
     return tuple(getattr(settings, "IMAGE_PICTURE_URL_PREFIXES", {}))
 
